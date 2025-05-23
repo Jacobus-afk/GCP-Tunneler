b202ad69a010cb2e0454fa61583083e8621b5c6e
Cobus Linde <barendl@staycontactable.com>
2025-05-23 21:52:56 +0200

    feat(main): print version if flag is set

--- cmd/gcp_tunneler/main.go
+++ cmd/gcp_tunneler/main.go
@@ -10,6 +10,8 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+var version = "undefined"
+
 func main() {
 	configureLogger()
 
@@ -20,7 +22,19 @@ func main() {
 		"whether or not to repopulate the instance list from GCP",
 	)
 
+	verFlag := flag.Bool(
+		"version",
+		false,
+		"print the current program version",
+	)
+
 	flag.Parse()
+
+	if *verFlag {
+		printVersion()
+		os.Exit(0)
+	}
+
 	envCfg := config.GetConfig()
 
 	if envCfg.Develop.Debug {
@@ -36,6 +50,10 @@ func main() {
 	if err != nil {
 		log.Fatal().Err(err).Msg("application failed")
 	}
+}
+
+func printVersion()  {
+	os.Stdout.WriteString(version + "\n")
 }
 
 func configureLogger() {
